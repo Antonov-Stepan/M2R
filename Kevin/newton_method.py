5c019435528e27e60295beff917dd2e5a8d5e80c
kdl23 <149395224+KevinDZLiu@users.noreply.github.com>
2025-06-05 16:55:00 +0100

    added second newton function, needs fixing

--- Kevin/newton_method.py
+++ Kevin/newton_method.py
@@ -36,8 +36,9 @@ def f(u, k, k_sign, c, amplitude):
     return np.append(f, constraint)
 
 
-def jacobian(N, u, k, k_sign, c, amplitude, res):
+def jacobian(u, k, k_sign, c, amplitude, res):
     """Computes the Jacobian"""
+    N = len(u)
     delta = 10**-7
     J = np.zeros((N+1, N+1))
 
@@ -60,13 +61,13 @@ def newton_meth(amp):
     """Solve the travelling wave solution with newton's method"""
 
     # create grid
-    N = 100
+    N = 1000
     L = np.pi
     X = np.linspace(-L, L, num=N, endpoint=False)
     dx = X[1] - X[0]
 
     # initial guess
-    u = amp*np.cos(X)
+    u = (amp/2)*np.cos(X)
     plt.plot(X, u, label="Initial Guess")
     c = 1.0
     k = np.fft.fftfreq(N, d=dx) * 2 * np.pi
@@ -76,7 +77,7 @@ def newton_meth(amp):
 
     # newton
     while err > 10**-10:
-        J = jacobian(N, u, k, k_sign, c, amp, res)
+        J = jacobian(u, k, k_sign, c, amp, res)
         corr = np.linalg.solve(-J, res)
         u += corr[:-1]
         c += corr[-1]
@@ -94,4 +95,51 @@ def newton_meth(amp):
     print(f"c estimate:{c}")
 
 
-newton_meth(1)
+def newton_meth2(amp):
+    """Only solving half the solution"""
+
+    # create grid
+    N = 100
+    # h = half
+    Nh = N//2 + 1
+    L = np.pi
+    X = np.linspace(-L, L, num=N, endpoint=False)
+    x = np.linspace(-L, 0, num=Nh, endpoint=True)
+    dx = x[1] - x[0]
+
+    # initial guess
+    u = (amp/2)*np.cos(x)
+    # full initial
+    uf = np.concatenate((u, u[-2:0:-1]))
+    plt.plot(X, uf, label="Initial Guess")
+    c = 1.0
+    k = np.fft.fftfreq(Nh, d=dx) * 2 * np.pi
+    k_sign = np.sign(k)
+    res = f(u, k, k_sign, c, amp)
+    err = np.max(np.abs(res))
+
+    # newton
+    while err > 10**-10:
+        J = jacobian(u, k, k_sign, c, amp, res)
+        corr = np.linalg.solve(-J, res)
+        u += corr[:-1]
+        c += corr[-1]
+        res = f(u, k, k_sign, c, amp)
+        err = np.max(np.abs(res))
+
+    # final u
+    uf = np.concatenate((u, u[-2:0:-1]))
+    plt.plot(X, uf, label="Solution")
+    plt.title(f"Travelling wave solution with {N} grid points")
+    plt.xlabel("X")
+    plt.ylabel("u")
+    plt.legend()
+    plt.grid(True)
+    # plt.plot(X, f(u, k, k_sign, c, amp)[:-1], label="f")
+    plt.show()
+    print(f"c estimate:{c}")
+
+
+amp = 0.1
+newton_meth(amp)
+newton_meth2(amp)
