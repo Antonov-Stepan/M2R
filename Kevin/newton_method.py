6d9b30257f860b4f0402b60fa28a2c6c8fc92434
kdl23 <149395224+KevinDZLiu@users.noreply.github.com>
2025-06-06 16:14:20 +0100

    made function have initial guess as parameter, started bifurcation function

--- Kevin/newton_method.py
+++ Kevin/newton_method.py
@@ -99,12 +99,10 @@ def jacobian2(u, c, amplitude, res):
     return J
 
 
-def newton_meth(amp):
+def newton_meth(N, L, amp):
     """Solve the travelling wave solution with newton's method"""
 
     # create grid
-    N = 512
-    L = np.pi
     X = np.linspace(-L, L, num=N, endpoint=False)
     dx = X[1] - X[0]
 
@@ -137,39 +135,44 @@ def newton_meth(amp):
     print(f"c estimate:{c}")
 
 
-def newton_meth2(amp):
-    """Only solving half the solution"""
+def newton_meth2(N, u, amp, c=1.0):
+    """Only solving half the solution but return full reflected solution"""
 
     # create grid
-    N = 512
     # h = half
     Nh = N//2 + 1
-    L = np.pi
-    X = np.linspace(-L, L, num=N, endpoint=False)
-    x = np.linspace(-L, 0, num=Nh, endpoint=True)
 
     # initial guess
-    u = (amp/2)*np.cos(x)
-    # full initial
-    uf = np.concatenate((u, u[-2:0:-1]))
-    plt.plot(X, uf, label="Initial Guess")
-    c = 1.0
-    res = f2(u, c, amp)
+    ui = np.copy(u)
+    ui = ui[0:Nh]
+    res = f2(ui, c, amp)
     err = np.max(np.abs(res))
 
     # newton
     while err > 10**-10:
-        J = jacobian2(u, c, amp, res)
+        J = jacobian2(ui, c, amp, res)
         corr = np.linalg.solve(-J, res)
-        u += corr[:-1]
+        ui += corr[:-1]
         c += corr[-1]
-        res = f2(u, c, amp)
+        res = f2(ui, c, amp)
         err = np.max(np.abs(res))
 
     # final u
-    uf = np.concatenate((u, u[-2:0:-1]))
-    plt.plot(X, uf, label="Solution")
-    plt.title(f"Travelling wave solution with {N} grid points")
+    uf = np.concatenate((ui, ui[-2:0:-1]))
+    return uf, c
+
+
+def plot(N, L, ui, u, c):
+    """
+    Graphing initial guess and solution
+
+    ui: initial guess
+    u: solution
+    """
+    X = np.linspace(-L, L, num=N, endpoint=False)
+    plt.plot(X, ui, label="Initial Guess")
+    plt.plot(X, u, label="Solution")
+    plt.title(f"Travelling wave solution with {N} grid points, method 2")
     plt.xlabel("X")
     plt.ylabel("u")
     plt.legend()
@@ -179,6 +182,38 @@ def newton_meth2(amp):
     print(f"c estimate:{c}")
 
 
+def bifurcation(N, amp, n, u):
+    """Create bifurcation diagram for amplitude and c"""
+
+    ui = np.copy(u)
+    c = 1.0
+    step = amp/n
+    c_values = np.zeros(n)
+    h_values = np.zeros(n)
+    for i in range(n):
+        amplitude = step * (i + 1)
+        ui, c = newton_meth2(N, ui, amplitude, c=c)
+        # print(c)
+        c_values[i] = c
+        h_values[i] = amplitude
+
+    plt.plot(c_values, h_values)
+    plt.scatter(c_values, h_values)
+    plt.title("Bifurcation diagram")
+    plt.xlabel("c")
+    plt.ylabel("amplitude")
+    plt.grid(True)
+    plt.show()
+
+
+N = 512
+L = np.pi
 amp = 0.1
-newton_meth(amp)
-newton_meth2(amp)
+X = np.linspace(-L, L, num=N, endpoint=False)
+ui = (amp/2)*np.cos(X)
+
+newton_meth(N, L, amp)
+u, c = newton_meth2(N, ui, amp)
+print(len(ui))
+plot(N, L, ui, u, c)
+bifurcation(N, amp, 100, ui)
